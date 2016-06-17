b754a6ce33e5da8c37fa858e9c462028d9643601
Dax <limeside@gmail.com>
2016-06-16 20:22:22 -0500

    Add new post page, links to and from. :rabbit:

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@ var express = require('express'),
     User    = require('../models/user');
 
 router.route('/')
+  // Show indexed posts, associated users, and content.
   .get( (req, res) => {
     Post
       .fetchAll({withRelated: ['users']})
@@ -12,9 +13,40 @@ router.route('/')
 
         res.render('posts/index', {posts: posts, users: posts.users});
       });
+  })
+  //Post a new blog post.
+  .post( (req, res) => {
+    var post = req.body;
+
+    Post({
+      post:post.user_id,
+      post:post.title,
+      post:post.post
+    },'id')
+      .save()
+      .then( () => {
+        res.redirect('/posts');
+      })
+  })
+  //Edit a post.
+  .put( (req, res) => {
+
   });
 
+router.route('/new')
+// Show new post page, list available users to post as. (Can't auth yet, #rip)
+.get( (req, res) => {
+  User
+  .fetchAll()
+  .then( results => {
+    var users = results.toJSON();
+
+    res.render('posts/new', { users : users });
+  });
+});
+
 router.route('/:post_id')
+  // Show post by id and associated user comments.
   .get( (req, res) => {
     var id = req.params.post_id;
     Post
@@ -22,6 +54,7 @@ router.route('/:post_id')
       .fetch({withRelated: ['users', 'comments']})
       .then( results => {
         var post = results.toJSON()
+            // After finding associated comments, find users associated with commentid.
             comments = results.related('comments')
               .fetch({withRelated:['users']})
               .then( commentResult => {
@@ -31,6 +64,24 @@ router.route('/:post_id')
 
               });
       });
+  })
+  // Post a new comment.
+  .post( (req, res) => {
+
+  })
+
+  // Edit a post's content.
+  .put( (req, res) => {
+
+  });
+
+
+router.route('/:post_id/comment')
+  //Show new comment page, and list available users to post as.
+  .get( (req, res) => {
+    var post_id = req.params.post_id;
+
+    //Get post by ID, fetchall users.
   });
 
 
